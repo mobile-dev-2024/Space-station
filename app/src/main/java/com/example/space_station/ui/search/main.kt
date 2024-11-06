d83c30e642f17592bbaed3103d8401aef13c205f
newdongjun <shindj4613@outlook.com>
2024-11-06 15:49:42 +0900

    search page init

---
+++ app/src/main/java/com/example/space_station/ui/search/main.kt
@@ -0,0 +1,25 @@
+package com.example.space_station.ui.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.space_station.Greeting
+import com.example.space_station.ui.theme.SpacestationTheme
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SpacestationTheme {
+    }
+}
