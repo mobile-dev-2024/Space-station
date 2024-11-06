9426d0c04ba3f3f7c791c17eda37ad8c3f934682
newdongjun <shindj4613@outlook.com>
2024-11-07 01:07:52 +0900

    디자인 수정

--- app/src/main/java/com/example/space_station/ui/search/building.kt
+++ app/src/main/java/com/example/space_station/ui/search/building.kt
@@ -1,7 +1,6 @@
 package com.example.space_station.ui.search
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,15 +12,13 @@ import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.space_station.ui.theme.SpacestationTheme
 import com.example.space_station.viewmodel.LectureTimetable
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -50,6 +47,7 @@ fun Buildings(
                 .fillMaxSize()
                 .padding(innerPadding)
                 .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             items(lectureTimetable.buildings.value) { buildingName ->
                 BuildingCard(buildingName, onClick = {
@@ -73,9 +71,9 @@ private fun BuildingCard(
     ) {
         Row (
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxSize().padding(32.dp)
+            modifier = Modifier.fillMaxSize().padding(24.dp)
         ){
-            Text(buildingName)
+            Text(buildingName, style = typography.titleLarge)
             Icon(
                 imageVector = Icons.Outlined.Star,
                 contentDescription = "Settings",
