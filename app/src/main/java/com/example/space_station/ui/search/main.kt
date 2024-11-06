024376728b2e39d3e36a863d14cf0ce5d5b0ac81
newdongjun <shindj4613@outlook.com>
2024-11-06 18:47:41 +0900

    add lib

--- app/src/main/java/com/example/space_station/ui/search/main.kt
+++ app/src/main/java/com/example/space_station/ui/search/main.kt
@@ -1,12 +1,42 @@
 package com.example.space_station.ui.search
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.space_station.Greeting
 import com.example.space_station.ui.theme.SpacestationTheme
 
+//서치페이지의 메인 페이지
 
 
+@Composable
+fun SearchMain() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "search"
+    ) {
+        composable(route = "search") {
+
+        }
+        composable(route = "buildings") {
+
+        }
+        composable(route = "floors") {
+
+        }
+        composable(route = "rooms") {
+
+        }
+    }
+
+
+}
 
 
 
@@ -19,7 +49,8 @@ import com.example.space_station.ui.theme.SpacestationTheme
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SearchPreview() {
     SpacestationTheme {
+        SearchMain()
     }
 }
