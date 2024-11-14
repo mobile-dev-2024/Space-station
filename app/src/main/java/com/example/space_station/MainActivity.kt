29102f217943f705f609fb30f0ed5a24995b23ed
hur2 <dhwoghks0626@naver.com>
2024-11-14 14:26:44 +0900

    시작시에시간표 초기화 함수 작동하도록 수정

--- app/src/main/java/com/example/space_station/MainActivity.kt
+++ app/src/main/java/com/example/space_station/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.example.space_station
 
-import android.R.attr.name
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.space_station.ui.search.Buildings
-import com.example.space_station.ui.search.SearchMain
 import com.example.space_station.ui.theme.SpacestationTheme
 import com.example.space_station.viewmodel.LectureTimetable
+import com.example.space_station.ui.search.SearchMain
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,19 +23,11 @@ class MainActivity : ComponentActivity() {
             SpacestationTheme {
                 val lectureTimetableViewModel = viewModel<LectureTimetable>()
                 lectureTimetableViewModel.loadExcelData(this)
+                lectureTimetableViewModel.loadExcelData2(this)
 
                 SearchMain(
                     lectureTimetable = lectureTimetableViewModel
                 )
-
-
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding),
-//                        lectureTimetable = lectureTimetableViewModel
-//                    )
-//                }
             }
         }
     }
